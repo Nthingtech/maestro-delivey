ac6d4f11d25de96b3b63ccd130d8e7b227e87a52
Fernando <store.nthing@gmail.com>
2025-12-19 21:30:15 -0300

    feat: TicketResources

---
+++ pizza-api/src/main/java/br/maestro/pizza/rs/TicketsResources.java
@@ -0,0 +1,28 @@
+package br.maestro.pizza.rs;
+
+import br.maestro.pizza.model.Ticket;
+import jakarta.ws.rs.Path;
+
+@Path("/tickets")
+public class TicketsResources {
+
+    public Ticket createTicket(Object param) {
+        return null;
+    }
+
+    public Ticket readTicket(Long id) {
+        return null;
+    }
+
+    public Ticket deleteTicket(Long id) {
+        return null;
+    }
+
+    public Ticket addItem(Long id, Object param) {
+        return null;
+    }
+
+    public Ticket submitTicket(Long id) {
+        return null;
+    }
+}
