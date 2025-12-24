9ff8aef8bfa5d00629d2bc49c764abc5c2600c39
Fernando <store.nthing@gmail.com>
2025-12-24 01:08:27 -0300

    user security

--- pizza-api/src/main/java/br/maestro/pizza/rs/user/WhoAmIResource.java
+++ pizza-api/src/main/java/br/maestro/pizza/rs/user/WhoAmIResource.java
@@ -1,6 +1,9 @@
 package br.maestro.pizza.rs.user;
 
 import io.quarkus.logging.Log;
+import io.quarkus.security.identity.SecurityIdentity;
+import jakarta.annotation.security.RolesAllowed;
+import jakarta.inject.Inject;
 import jakarta.persistence.ManyToOne;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
@@ -12,11 +15,19 @@ import java.util.Map;
 @Path("/user/whoami")
 public class WhoAmIResource {
 
+    @Inject
+    SecurityIdentity identity;
+
     @GET
     @Produces(MediaType.APPLICATION_JSON)
+    @RolesAllowed("user")
     public Map<String, String > getWhoAmI() {
-        Map<String, String> result = Map.of();
-        Log.infof("WhoAmI() = %s", result);
+
+        var name = "anonymous";
+        if (!identity.isAnonymous()){
+            name = identity.getPrincipal().getName();
+        }
+        Map<String, String> result = Map.of("name", name);
         return result;
     }
 }
